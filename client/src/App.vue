f29098719a986a73b3a974e5be395cdbfede6970
Yannik Seitz <yannikseitz@outlook.de>
2020-07-29 14:36:33 +0200

    client vuex

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <AddMedia />
+      <FilterMedia />
+      <Media />
+    </div>
+  </div>
+</template>
+
+<script>
+import AddMedia from "./components/AddMedia.vue";
+import FilterMedia from "./components/FilterMedia.vue";
+import Media from "./components/Media.vue";
+
+export default {
+  name: "App",
+  components: {
+    AddMedia,
+    FilterMedia,
+    Media,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
---
+++ client/src/components/Media.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <h1>Medien</h1>
+    <hr />
+    <p class="error-p" v-if="getError">{{ getError }}</p>
+    <div class="media-container">
+      <div class="media" v-for="media in getAllMedia" :key="media._id" @dblclick="deleteMedia(media._id)">
+        <p class="text">{{ media.name }}</p>
+        {{ `${media.createdAt.getDate()}/${media.createdAt.getMonth() + 1}/${media.createdAt.getFullYear()}` }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//import MediaService from "../MediaService";
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Media",
+  methods: {
+    ...mapActions(["fetchMedia", "deleteMedia", "updateMedia", "setError"]),
+    // onDblClick(todo) {
+    //   const updTodo = {
+    //     id: todo.id,
+    //     title: todo.title,
+    //     completed: !todo.completed,
+    //   };
+    //   this.updateTodo(updTodo);
+    // },
+  },
+  computed: {
+    ...mapGetters(["getAllMedia", "getError"]),
+  },
+  created() {
+    try {
+      this.fetchMedia();
+    } catch (err) {
+      this.setError(err.message);
+    }
+  },
+};
+
+// data() {
+//   return {
+//     media: [],
+//     error: "",
+//   };
+// },
+// async created() {
+//   try {
+//     this.media = await MediaService.getMedia();
+//   } catch (err) {
+//     this.error = err.message;
+//   }
+// },
+// methods: {
+// async createMedia() {
+//   await MediaService.insertMedia(this.inputText);
+//   this.media = await MediaService.getMedia();
+//   this.inputText = "";
+// },
+// async deleteMedia(id) {
+//   await MediaService.deleteMedia(id);
+//   this.media = await MediaService.getMedia();
+// },
+</script>
+
+<style scoped>
+.media-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.media {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
