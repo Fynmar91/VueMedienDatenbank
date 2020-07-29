6c527770739c0d5098ddfb940af026c34dc5623f
Yannik Seitz <yannikseitz@outlook.de>
2020-07-29 10:06:39 +0200

    1

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ const posts = require("./routes/api/posts");
 app.use("/api/posts", posts);
 
 // Handle production
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   // Static folder
-  app.use(express.static(__dirname + "/public"));
+  app.use(express.static(__dirname + "/public/"));
 
   // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
