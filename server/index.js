0d4fafa65eb04c848115b1b962c644bb3d17e36d
Yannik Seitz <yannikseitz@outlook.de>
2020-07-29 10:19:24 +0200

    1

--- server/index.js
+++ server/index.js
@@ -14,11 +14,15 @@ app.use("/api/posts", posts);
 // Handle production
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
+  console.log("production === true");
+
   // Static folder
   app.use(express.static(__dirname + "/public/"));
 
   // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+} else {
+  console.log("production === false");
 }
 
 const port = process.env.PORT || 5000;
